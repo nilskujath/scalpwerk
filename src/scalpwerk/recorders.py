a122a40d0681d03f62cba18a70a3c58f9ec56d01
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2026-05-27 20:28:30 +0200

    feat: added PicklRecorder to recorders module

--- src/scalpwerk/recorders.py
+++ src/scalpwerk/recorders.py
@@ -1,10 +1,37 @@
 import json
+import pickle
 
 from dataclasses import asdict
-from io import TextIOWrapper
+from io import BufferedWriter, TextIOWrapper
 from pathlib import Path
 
 from .core import EventBase, RecorderBase
+
+
+class PickleRecorder(RecorderBase):
+    def __init__(self, run_id: str, runs_dir_path: Path = Path("./runs")) -> None:
+        self._run_id = run_id
+        self._pkl_path = runs_dir_path / self._run_id / "events.pkl"
+        self._pkl_file: BufferedWriter | None = None
+        super().__init__()
+
+    @property
+    def path(self) -> Path:
+        return self._pkl_path
+
+    def _event_loop(self) -> None:
+        self._pkl_path.parent.mkdir(parents=True, exist_ok=True)
+        self._pkl_file = open(self._pkl_path, "wb")
+        try:
+            super()._event_loop()
+        finally:
+            if self._pkl_file is not None:
+                self._pkl_file.close()
+
+    def _on_event(self, event: EventBase) -> None:
+        assert self._pkl_file is not None
+        pickle.dump(event, self._pkl_file)
+        self._pkl_file.flush()
 
 
 class JSONLRecorder(RecorderBase):
